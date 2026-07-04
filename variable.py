686ba4765c3cc2f7d85f64bf83acdb25d95f06c1
Arnav <aarnavsalvi2007@gmail.com>
2026-07-04 13:56:30 +0530

    Python

---
+++ variable.py
@@ -0,0 +1,26 @@
+
+# import copy
+lst = [ 4,2,3,7,8,9 ]
+# tup = ( 1,2,3 )
+# sets = { 3,2,1 }
+
+# print (tup)
+# print (sets)
+
+# lst.append(4)
+# lst.insert(2,9) 
+# lst[0] = 7
+# lst.remove(3)
+# lst.pop(1)
+# lst.clear()
+# lst2 = lst.copy()
+# # lst2.sort()
+# lst2.sort(reverse=True)
+# lst2 = lst.copy()
+# lst2[3][0] = 9
+# lst2 = copy.deepcopy(lst)
+# lst2[3][0] = 9
+ 
+
+print(lst[::2])
+
