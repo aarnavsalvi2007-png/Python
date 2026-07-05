66180d7893cace715e04449f07a87025385ad434
Arnav <aarnavsalvi2007@gmail.com>
2026-07-05 14:06:26 +0530

    python

---
+++ conditional.py
@@ -0,0 +1,25 @@
+num =45
+# if num % 2 == 0:
+
+#  print("Even number")
+
+# elif num % 2 != 0:
+
+
+#  print(" odd number")
+
+# if num > 0:
+#     print(" + number")
+# elif num < 0:
+#     print( " - number ")
+
+# if num > 50:
+#     print(" Greater than 50 ")
+# elif num < 50 and num >= 0:
+#     print(" Less than 50 ")
+# else:
+#     print(" Negative number ")
+
+
+
+    
---
+++ typecasting.py
@@ -0,0 +1,30 @@
+# num = 1
+# num2 = str(num)
+# print(num2)
+# num2 = 2.6
+# print(num2)
+# num3 = int(num2)
+# print(num3)
+
+# name = """If you have an ASUS ROG Strix laptop
+# , you should almost certainly download the Windows x64 (64-bit) installer, not ARM64. ASUS ROG Strix gaming laptops use Intel or AMD Ryzen processors,
+# which are x64-based"""
+# print(name)
+# name1 = "arnav"
+# print(name1[0])
+
+# temp = "Hello World"
+# print(temp[1:7])
+# print(temp[:6])
+# print(temp[2:])
+# temp=temp.upper()
+# temp=temp.lower()
+# print(temp)
+
+# name = "  Hello       "
+# print(name.strip()) # remove spaces from starting and beginning
+# name=name.replace("l","k")
+# print(name)
+firstname= "Arnav"
+lastname= "Salvi"
+print(firstname + " " + lastname)
